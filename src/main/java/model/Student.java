9ac5442d04c0328a9305c20ae385784c11194415
Konan-hub <yawokonan28@gmail.com>
2026-06-05 20:15:34 +0200

    "Implement Student class with input validation"

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,4 +1,39 @@
 package model;
 
 public class Student {
+    private String name ;
+    private int age ;
+
+    public Student(String name, int age) {
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+            return age;
+        }
+
+    public void setName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name is invalid!");
+        }
+        if ( !name.matches("\\p{L}+")) {
+            throw new IllegalArgumentException("Name must contain only letters!");
+        }
+        this.name = name;
+
+            }
+
+    public void setAge(int age) {
+        if ( age <= 0 || age > 130 ) {
+            throw new IllegalArgumentException("Age must be between 1 and 130 ");
+
+        }
+        this.age = age;
+    }
 }
+
